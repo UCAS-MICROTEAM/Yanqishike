7a12132871275b2314fe1dd965c64158f70de4f4
mixiaoxiaogogo@163.com <mixiaoxiaogogo@163.com>
2017-12-11 09:09:52 +0000

    首页内容

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
-   
+    def index
+      @uuu = User.first(2)
+    #   @all_cards = Card.all
+        # if user_signed_in? 
+            # @index_statuses =  current_user.statuses.last(3)
+        # end
+        # 近期的动态（美食记）
+        @index_statuses =  Status.last(6).reverse!
+        # 近期的美食单
+        # @index_foodpapers = Foodpaper.last(6).reverse!
+        # 小卡片
+        @index_cards = Card.last(6).reverse!
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,41 @@
 # 再一种写法
 # t=Time.now
 # Blog.create(:Title=>"中文第一版",:Created=>t,:Content=>"真实的    中文第一版")
-if User.first == nil 
-    user1 = User.create(:name => "wangbin", :password => "wangbin", :role => 0)
+# if User.first == nil 
+    # user1 = User.create(:name => "wangbin", :password => "wangbin", :role => 0)
+user1 = User.first
+if user1 == nil
+    # return
+    puts "请先在网站上注册一个用户！！！"
+end
+
+if Merchant.first == nil
     merchant1 = Merchant.create(:name => "商家1" , :address => "我是商家地址", :introduce => "我是商家介绍", :user => user1 )
     food1 = Food.create(:name => "菜品1", :user => user1, :merchant => merchant1 ) 
 else
-    user1 = User.first
     merchant1 = Merchant.first
     food1 = Food.first
 end
 
-if Status.first == nil
-    status1 = Status.create(:user => user1, :food => food1, :content => "我是发布的内容内容内容")
+if Status.count <= 2
+    for i in 0...20
+        # puts "局部变量的值为 #{i}"
+        status1 = Status.create(:user => user1, :food => food1, :content => "我是发布的内容内容内容#{i}")
+    end 
+    # status1 = Status.create(:user => user1, :food => food1, :content => "我是发布的内容内容内容")
 else
     status1 = Status.first
 end
 
-if Card.first == nil
-    card1 = Card.create(:user => user1, :content => "我是小卡片的内容")
+# if Foodpaper.first = nil
+#   foodpaper = Foodpaper.create(:user => user1, :content => "我是Foodpaper的内容")
+# end
+
+if Card.count < 3
+    for i in 0...20
+        card1 = Card.create(:user => user1, :content => "我是小卡片的内容#{i}")
+         puts "创建小卡片 #{card1.content}"
+    end 
 else 
     card1 = Card.first
 end

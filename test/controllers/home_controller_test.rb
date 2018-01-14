22fb1ca8e451b7c0fff64562106dfa9818e49b24
Falcon <kouyingshuai@163.com>
2018-01-14 06:33:53 +0000

    添加controller测试

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
-class HomeControllerTest < ActionController::TestCase
+#class HomeControllerTest < ActionController::TestCase
+class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get :index
+    get users_url
     assert_response :success
   end
-
 end
+
+
